a6076755ca41af7dc3bcc4116c2cbb9d210b1a0f
Yuan Cui <yuan.cui@wetaworkshop.co.nz>
2018-02-13 16:11:49 +1300

    final version

--- ftrack_plugins/quickly_add_notes/quick_notes.py
+++ ftrack_plugins/quickly_add_notes/quick_notes.py
@@ -14,9 +14,10 @@ APPDATA = os.path.dirname(os.getenv('APPDATA'))
 class quick_notes_gui:
     '''GUI for quick notes'''
 
-    def __init__(self, app):
+    def __init__(self, app, auto_record=True):
         '''set GUI'''
         self.login_font = Font(family="New Times Roman", size=11)
+        self.wait_font = Font(family='Fixdsys',size=13)
         self.task_frame_count = 0
         self.submit_info = {}
         self.save_message = collections.OrderedDict()
@@ -28,63 +29,91 @@ class quick_notes_gui:
         self.session = None
         self.app = app
         self.search_labelframe = None
+        self.dialog = None
         # destroy previous GUI first
         for widget in app.winfo_children():
             widget.destroy()
         app.title("Quick notes")
         app.geometry('1400x800')
-        canvas = Canvas(app, borderwidth=0)
-        frame = Frame(canvas)
-        vsb = Scrollbar(app, orient="vertical", command=canvas.yview)
-        canvas.configure(yscrollcommand=vsb.set)
+        app.iconbitmap("./write.ico")
+        self.canvas = Canvas(app)
+        self.frame = Frame(self.canvas)
+        vsb = Scrollbar(app, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=vsb.set)
         vsb.pack(side="right", fill="y")
-        canvas.pack(side="left", fill="both", expand=True)
-        canvas.create_window((4,4), window=frame, anchor="nw")
-        frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas.create_window((4,4), window=self.frame, anchor="nw")
+        self.frame.bind("<Configure>", lambda event, canvas=self.canvas: onFrameConfigure(self.canvas))
+        self.frame.bind_all("<MouseWheel>", self._on_mousewheel)
         menu = Menu(app)
         app.config(menu=menu)
         fileMenu = Menu(menu, tearoff=False)
         menu.add_cascade(label='File', menu=fileMenu)
-        fileMenu.add_command(label='New', command=lambda app =app:self.new_project(app))
+        fileMenu.add_command(label='New', command=lambda app=app: self.new_project(app))
         fileMenu.add_command(label='Save', command=self.save_project)
         fileMenu.add_command(label='Load', command=self.load_project)
+        # mark bind cursor_busy
         fileMenu.add_separator()
-        fileMenu.add_command(label='Quit', command=lambda app=app:self.quit(app))
-        self.frame1 = Frame(frame)
+        fileMenu.add_command(label='Quit', command=lambda app=app: self.quit(app))
+        self.frame1 = Frame(self.frame)
         self.frame1.pack(side=TOP, fill=X, pady=20)
+        self.choose_user_entry_text = StringVar()
+        self.choose_key_entry_text = StringVar()
+        self.choose_url_entry_text = StringVar()
         self.choose_user_label = Label(self.frame1, text='User name:')
-        self.choose_user_entry = Entry(self.frame1)
+        self.choose_user_entry = Entry(self.frame1, textvariable=self.choose_user_entry_text)
         self.choose_key_label = Label(self.frame1, text='API Key:')
-        self.choose_key_entry = Entry(self.frame1)
+        self.choose_key_entry = Entry(self.frame1, textvariable=self.choose_key_entry_text, show="*")
         self.choose_url_label = Label(self.frame1, text='Server URL:')
-        self.choose_url_entry = Entry(self.frame1)
-        self.login_button = Button(self.frame1, text='Login', command=self.login)
-        self.login_err_msg = Label(self.frame1, text='Sorry: invalid login info')
+        self.choose_url_entry = Entry(self.frame1, textvariable=self.choose_url_entry_text)
         self.choose_url_label.pack(side=LEFT, padx=10)
         self.choose_url_entry.pack(side=LEFT, padx=10)
         self.choose_user_label.pack(side=LEFT, padx=10)
         self.choose_user_entry.pack(side=LEFT, padx=10)
         self.choose_key_label.pack(side=LEFT, padx=10)
         self.choose_key_entry.pack(side=LEFT, padx=10)
+        # auto remember login info
+        if auto_record:
+            try:
+                with open(APPDATA+'\\Local\\save.p', 'rb') as f:
+                    save_info = pickle.load(f)
+                    user_name = save_info['user_name']
+                    server_url = save_info['server_url']
+                    self.api_key = save_info['api_key']
+                    self.choose_user_entry_text.set(user_name)
+                    self.choose_key_entry_text.set(self.api_key)
+                    self.choose_url_entry_text.set(server_url)
+            except Exception:
+                pass
+        self.login_button = Button(self.frame1, text='Login', command=self.login)
+        self.login_button.bind("<Button-1>", self.cursor_busy)
+        # self.login_button.pack_configure(after=self.choose_key_entry)
+        self.login_err_msg = Label(self.frame1, text='Sorry: invalid login info', fg='red')
+        self.load_button = Button(self.frame1, text='Load previous session', bg='yellow', command=self.load_project)
+        self.load_button.bind("<Button-1>", self.cursor_busy)
         self.login_button.pack(side=LEFT, padx=10)
-        self.frame2 = Frame(frame)
+        # self.load_button.pack_configure(after=self.login_button)
+        self.load_button.pack(side=LEFT, padx=10)
+        self.frame2 = Frame(self.frame)
         self.search_task_label = Label(self.frame2, text='Enter task name:')
         self.search_task_text = StringVar()
         self.search_task_entry = Entry(self.frame2, textvariable=self.search_task_text)
         self.search_task_entry.bind('<Return>', self.search_task_enter)
         self.search_task_button = Button(self.frame2, text='Search', command=self.search_task)
+        self.search_task_button.bind('<Button-1>', self.cursor_busy)
         self.search_task_label.grid(row=0, column=0, padx=10)
         self.search_task_entry.grid(row=0, column=1, padx=10)
         self.search_task_button.grid(row=0, column=2, padx=10)
         self.search_labelframe = LabelFrame(self.frame2, text="Search Result ---- Click to add task")
-        self.frame3 = Frame(frame)
+        self.frame3 = Frame(self.frame)
         self.submit_button = Button(self.frame3, text='submit', bg='green', command=self.submit)
-        self.frame4 = Frame(frame)
+        self.frame4 = Frame(self.frame)
         self.choose_project_label = Label(self.frame4, text='Project name:')
         self.choose_project_text = StringVar()
         self.choose_project_entry = Entry(self.frame4, textvariable=self.choose_project_text)
         self.choose_project_entry.bind('<Return>', self.search_project_enter)
         self.search_project_button = Button(self.frame4, text='search', command=self.search_project)
+        self.search_project_button.bind("<Button-1>", self.cursor_busy)
         self.search_project_labelframe = LabelFrame(self.frame4, text="Search Result ---- Click to choose a project")
         self.choose_project_label.grid(row=0, column=0, padx=10)
         self.choose_project_entry.grid(row=0, column=1, padx=10)
@@ -93,24 +122,32 @@ class quick_notes_gui:
         self.k = ThreadJob(self.save, threading.Event(), AUTO_SAVE_TIME)
         app.protocol('WM_DELETE_WINDOW', lambda app=app:self.quit(app))
 
+
+    def _on_mousewheel(self, event):
+        '''mouse wheel event'''
+        if self.frame.winfo_height() > 800:
+            self.canvas.yview_scroll(-1*(event.delta/120), "units")
+
     def login(self):
         '''login'''
         self.login_err_msg.pack_forget()
         user_name = self.choose_user_entry.get()
         self.api_key = self.choose_key_entry.get()
         self.server_url = self.choose_url_entry.get()
-        user_name = 'yuan.cui'
-        self.api_key = 'afe3ba42-baba-46b1-b47b-35206f34488f'
-        self.server_url = 'http://192.168.9.82/'
         # unify url format
         if self.server_url and self.server_url[-1] == '/':
+            self.server_url = self.server_url[:-1]
+        if self.server_url and self.server_url[-1] == '.':
             self.server_url = self.server_url[:-1]
         try:
             self.session = get_session(self.server_url, self.api_key, user_name)
             self.user = get_user(self.session, user_name)
+            self.user_name = user_name
         except Exception:
             self.login_err_msg.pack(side=LEFT, padx=10)
             return
+        finally:
+            self.cursor_normal()
         self.pack_frame1()
         self.projects = self.session.query("Project").all()
         self.frame4.pack()
@@ -124,6 +161,7 @@ class quick_notes_gui:
         self.choose_key_entry.pack_forget()
         self.choose_url_label.pack_forget()
         self.choose_url_entry.pack_forget()
+        self.load_button.pack_forget()
         # self.submit_button.grid_remove()
         try:
             self.login_info.pack_forget()
@@ -148,13 +186,15 @@ class quick_notes_gui:
         for project in self.projects:
             if project_name.lower() in project['full_name'].lower():
                 project_button = Button(self.search_project_labelframe, text=project['full_name'])
-                project_button.bind("<ButtonPress-1>", lambda event:self.choose_project(event))
+                project_button.bind("<ButtonRelease-1>", self.choose_project)
+                project_button.bind("<ButtonPress-1>", self.cursor_busy)
                 project_button.grid(row=row, column=col, padx=10, pady=10)
                 col += 1
                 if col == 4:
                     col = 0
                     row += 1
         self.choose_project_text.set('')
+        self.cursor_normal()
 
     def change_project(self):
         """change a project"""
@@ -176,11 +216,13 @@ class quick_notes_gui:
         for project in self.projects:
             if project_name == project['full_name']:
                 self.project = project
+                self.project_id = self.project['id']
                 break
         self.tasks = get_tasks_from_project(self.project)
         self.search_project_labelframe.grid_remove()
         self.pack_frame4()
         self.pack_frame2()
+        self.cursor_normal()
 
     def pack_frame4(self):
         '''show frame4 in window'''
@@ -214,6 +256,7 @@ class quick_notes_gui:
                     col = 0
                     row += 1
         self.search_task_text.set('')
+        self.cursor_normal()
 
     def use_task(self, event):
         '''add tasks by click task name button'''
@@ -225,7 +268,7 @@ class quick_notes_gui:
         task_frame = Frame(self.frame3)
         task_name = event.widget.cget("text")
         if task_name in self.save_message.keys():
-            tkMessageBox.showinfo("Warning", "Task has already existed")
+            tkMessageBox.showwarning("Warning", "Task has already existed")
         else:
             task_name_label = Label(task_frame, text=task_name, width=50)
             task_delete_button = Button(task_frame, text='Delete')
@@ -286,19 +329,17 @@ class quick_notes_gui:
 
     def new_project(self, app):
         '''create new project'''
-        quick_notes_gui(app)
+        quick_notes_gui(app, False)
         self.k.stop()
 
     def save(self):
         '''save data in project'''
         if self.project and self.user and str(self.save_message) != 'OrderedDict()':
-            project_id = self.project['id']
-            user_name = self.user['username']
             with open(APPDATA+'\\Local\\save.p', 'wb') as f:
                 pickle.dump({
                     "server_url": self.server_url,
-                    "project_id": project_id,
-                    "user_name": user_name,
+                    "project_id": self.project_id,
+                    "user_name": self.user_name,
                     "api_key": self.api_key,
                     "notes": self.save_message
                 }, f)
@@ -310,26 +351,29 @@ class quick_notes_gui:
         if(self.save()):
             tkMessageBox.showinfo("Successful", "Saving successfully")
         else:
-            tkMessageBox.showinfo("Warning", "Nothing for saving!")
+            tkMessageBox.showwarning("Warning", "Nothing for saving!")
 
     def load_project(self):
         '''load project'''
+        self.cursor_busy(None)
         self.k.stop()
         self.k = ThreadJob(self.save, threading.Event(), AUTO_SAVE_TIME)
         try:
             with open(APPDATA+'\\Local\\save.p', 'rb') as f:
                 save_info = pickle.load(f)
         except Exception:
-            tkMessageBox.showinfo("Failure", "Loading failure!")
+            tkMessageBox.showwarning("Failure", "Loading failure!")
+            self.cursor_normal()
             return
-        project_id = save_info['project_id']
-        user_name = save_info['user_name']
+        self.project_id = save_info['project_id']
+        self.user_name = save_info['user_name']
         notes_msg = save_info['notes']
-        server_url = save_info['server_url']
-        api_key = save_info['api_key']
-        self.session = get_session(server_url, api_key, user_name)
-        self.project = self.session.query("Project where id is {0}".format(project_id)).one()
-        self.user = self.session.query("User where username is '{0}'".format(user_name)).one()
+        self.server_url = save_info['server_url']
+        self.login_err_msg.pack_forget()
+        self.api_key = save_info['api_key']
+        self.session = get_session(self.server_url, self.api_key, self.user_name)
+        self.project = self.session.query("Project where id is {0}".format(self.project_id)).one()
+        self.user = self.session.query("User where username is '{0}'".format(self.user_name)).one()
         self.tasks = get_tasks_from_project(self.project)
         self.projects = self.session.query("Project").all()
         self.save_message = notes_msg
@@ -374,6 +418,18 @@ class quick_notes_gui:
                 task_frame.pack(side=BOTTOM, fill=X, pady=5)
                 self.frame3.pack(side=TOP, fill=BOTH, pady=10, padx=15)
                 self.task_frame_count = len(notes_msg)
+        self.cursor_normal()
+
+
+    def cursor_busy(self, event):
+        self.canvas.config(cursor="wait")
+        self.app.title(">>>   Program loading! Please wait for a while!   <<< ")
+        self.app.iconbitmap("./loading.ico")
+
+    def cursor_normal(self):
+        self.canvas.config(cursor="")
+        self.app.title("Quick notes")
+        self.app.iconbitmap("./write.ico")
 
     def focusOut(self, event, task_name):
         '''save result when mouse move out of text area'''
@@ -384,11 +440,22 @@ class quick_notes_gui:
 
     def search_task_enter(self, event):
         '''enter key to search task'''
+        self.cursor_busy(event)
         return self.search_task()
 
     def search_project_enter(self, event):
         '''enter key to search project'''
+        self.cursor_busy(event)
         return self.search_project()
+
+    # def wait(self):
+    #     win = Toplevel(self.app)
+    #     win.transient()
+    #     win.geometry('400x150')
+    #     win.title('Loading...')
+    #     Label(win, text='Program is Loading now', font=self.wait_font, fg='red').pack(fill=X, pady=20)
+    #     Label(win, text='Please wait for a while!', font=self.wait_font, fg='red').pack(fill=X)
+    #     return win
 
 def get_tasks_from_project(project):
     '''pick tasks and milestones up from project'''
@@ -437,11 +504,11 @@ def submit_info_to_ftrack(info, session, user, project):
             session.commit()
             tkMessageBox.showinfo("Successful", "Successful submission!")
         except Exception:
-            tkMessageBox.showinfo("Failure", "Sorry, your notes are not submitted!")
+            tkMessageBox.showwarning("Warning", "Sorry, your notes are not submitted!")
             session.rollback()
             raise
     else:
-        tkMessageBox.showinfo("Warning", "No submission content!")
+        tkMessageBox.showwarning("Warning", "No submission content!")
 
 class ThreadJob(threading.Thread):
     '''for auto save job'''
@@ -476,6 +543,11 @@ class ThreadJob(threading.Thread):
 
     def stopped(self):
         return self._stop_event.is_set()
+
+
+
+
+
 
 def main():
     app = Tk()
